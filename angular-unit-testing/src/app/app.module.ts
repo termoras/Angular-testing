4e30c95cad33dec3e698a0ebb7fe38c4366c5f5f
Rasmus Termonen <31391711+termoras@users.noreply.github.com>
2019-09-22 10:08:09 +0300

    Initial commit

---
+++ angular-unit-testing/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    ContactComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
